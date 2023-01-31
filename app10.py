a337dfbc301e2114e5e05e0cf66f426a3d0ef594
Ani2599 <116948077+Ani2599@users.noreply.github.com>
2023-01-31 10:29:11 +0530

    Add files via upload

---
+++ app10.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from math import sqrt
+from sklearn.metrics import mean_squared_error
+from matplotlib import pyplot
+from statsmodels.tsa.seasonal import seasonal_decompose
+from statsmodels.tsa.holtwinters import SimpleExpSmoothing # SES
+from statsmodels.tsa.holtwinters import Holt # Holts Exponential Smoothing
+from statsmodels.tsa.holtwinters import ExponentialSmoothing # Holt Winter's Exponential Smoothing
+
+st.title('Forecast of Cement Sales')
+st.markdown('Sales Graph')
+
+uploaded_file = st.file_uploader(" ", type=['xlsx']) #Only accepts excel file format
+
+
+if uploaded_file is not None:     
+    cement = pd.read_excel(uploaded_file)
+    cement['Month'] = cement['Month'].apply(lambda x: x.strftime('%B-%Y'))
+    
+    
+    
+    hwe_model_mul_add = ExponentialSmoothing(cement["Sales"], seasonal = "mul", trend = "add", seasonal_periods = 12).fit()
+    newdata_pred = hwe_model_mul_add.predict(start = cement.index[0], end = cement.index[-1])
+    
+    st.info("Forecasted Value")
+
+    
+
+    st.subheader("For exponential model")
+   
+    st.write("Sales Forecast: ", newdata_pred)
+   
+    
+    st.subheader("Thanks for visit.")
+
